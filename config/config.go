dd83edca8ab614ac8081e58cc8c4e21ce3a98fb4
Eretic431 <abdullapower@outlook.com>
2022-01-16 12:24:01 +0300

    redis: added redis config

--- config/config.go
+++ config/config.go
@@ -4,11 +4,16 @@ import "github.com/caarlos0/env"
 
 type Config struct {
 	ServerCfg *ServerConfig
+	RedisCfg  *RedisConfig
 }
 
 type ServerConfig struct {
 	Production bool   `env:"PRODUCTION" envDefault:"false"`
 	Port       string `env:"PORT" envDefault:":8080"`
+}
+
+type RedisConfig struct {
+	Url string `env:"REDIS_URL" envDefault:"redis:6379"`
 }
 
 func GetConfig() (*Config, error) {
@@ -17,5 +22,13 @@ func GetConfig() (*Config, error) {
 		return nil, err
 	}
 
-	return &Config{ServerCfg: sCfg}, nil
+	rCfg := &RedisConfig{}
+	if err := env.Parse(rCfg); err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		ServerCfg: sCfg,
+		RedisCfg:  rCfg,
+	}, nil
 }
